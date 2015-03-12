7c8467cb2a677b7e2b2cb3157e43aa00b8e5158d
Marcio Carvalho <marciosdc@gmail.com>
2015-03-12 20:08:00 -0300

    Cadastro de Instituição

---
+++ Academico/Academico/JavaSource/br/edu/ifba/academico/business/cadastros/Instituicao.java
@@ -0,0 +1,43 @@
+package br.edu.ifba.academico.business.cadastros;
+
+public class Instituicao 
+{
+	private String nome;
+	private String endereco;
+	private String logo;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public String getLogo() {
+		return logo;
+	}
+	
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
---
+++ Academico/Academico/JavaSource/br/edu/ifba/academico/business/cadastros/InstituicaoDAO.java
@@ -0,0 +1,18 @@
+package br.edu.ifba.academico.business.cadastros;
+
+import java.util.List;
+
+public class InstituicaoDAO 
+{
+	public static void save(Instituicao instituicao)
+	{
+		// JDBC
+		System.out.println("Instituição Salva no Banco de Dados!");
+	}
+	
+	public List<Instituicao> getCollection()
+	{
+		
+		return null;
+	}
+}
