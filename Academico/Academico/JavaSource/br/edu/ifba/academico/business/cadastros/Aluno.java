9a28e3d0c0e66ca9571d543780ec43820b94f1f7
marciosdc <marciosdc@gmail.com>
2015-03-20 11:17:37 -0300

    Update Aluno.java

--- Academico/Academico/JavaSource/br/edu/ifba/academico/business/cadastros/Aluno.java
+++ Academico/Academico/JavaSource/br/edu/ifba/academico/business/cadastros/Aluno.java
@@ -6,10 +6,7 @@ public class Aluno
 	private String endereco;
 	private String teste;
 	private String ola;
-<<<<<<< HEAD
 	private String marcio;
-=======
->>>>>>> refs/remotes/origin/master
 	private String carvalho;
 
 	public String getNome()
