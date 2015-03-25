8f1adabdcfcc8cfe0fefe946805cf940db8b47a6
jpmoraes <morais566@gmail.com>
2015-03-25 15:30:27 -0300

    Tela Cadastro Departamento

---
+++ Academico/Academico/JavaSource/br/edu/ifba/academico/business/cadastros/DepartamentoDAO.java
@@ -0,0 +1,18 @@
+package br.edu.ifba.academico.business.cadastros;
+
+import java.util.List;
+
+public class DepartamentoDAO {
+
+	public static void save(Departamento departamento)
+	{
+		// JDBC
+		System.out.println("Departamento Salva no Banco de Dados!");
+	}
+	
+	public List<Departamento> getCollection()
+	{
+		
+		return null;
+	}
+}
